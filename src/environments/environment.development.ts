b5ef5c9de12bada3bf6df45a2cbacc77130f0fa2
Selvaraju Rethinam <Selvaraju.Rethinam@quesscorp.com>
2026-02-24 08:48:16 +0530

    Other Income Invoice culture added

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production:false,
-    //apiUrl:"https://localhost:7087/api/",
+    apiUrl:"https://localhost:7087/api/",
   
-    // apiUrl:"https://qpayerp-api.quesscorp.com/api/"
-     apiUrl:"https://psdautomationapi-dev.quesscorp.com/api/"
+    //apiUrl:"https://qpayerp-api.quesscorp.com/api/"
+     //apiUrl:"https://psdautomationapi-dev.quesscorp.com/api/"
 };
