178df69a9faeb3d598e1d678c128ba84dfc8008d
monika10037659 <monika.ramesh@quesscorp.com>
2026-07-27 19:22:20 +0530

    categorychange ui binding search and update

--- src/app/pages/admin/categorychange/categorychange.component.ts
+++ src/app/pages/admin/categorychange/categorychange.component.ts
@@ -1,17 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, InjectionToken, ViewChild } from '@angular/core';
 import { MatCardModule } from "@angular/material/card";
 import { CompanyallComponent } from "../../../common/CompanyAll/companyall.component";
 import { MatIconModule } from '@angular/material/icon';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Payperiodclass } from '../../../Models/Common';
 import { PayPeriodComponent } from '../../../common/payperiod/payperiod.component';
+import { CategoryChangeService } from '../../../Service/Admin/category-change.service';
+import { ICategoryChange } from '../../../Repository/Admin/ICategoryChange';
+import { MatDialog } from '@angular/material/dialog';
+import { EncryptionService } from '../../../Shared/encryption.service';
+import { SessionStorageService } from '../../../Shared/SessionStorageService';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSort } from '@angular/material/sort';
+export const Pay_TOKEN = new InjectionToken<ICategoryChange>('Pay_TOKEN');
 
 @Component({
   selector: 'app-categorychange',
-  imports: [MatCardModule, CompanyallComponent, MatIconModule, FormsModule, CommonModule, PayPeriodComponent],
+  imports: [MatCardModule, CompanyallComponent, MatIconModule, FormsModule, CommonModule, PayPeriodComponent, ReactiveFormsModule, MatIconModule, MatTooltipModule, MatTableModule, MatPaginatorModule],
   templateUrl: './categorychange.component.html',
-  styleUrl: './categorychange.component.css'
+  styleUrl: './categorychange.component.css',
+  providers: [
+    {
+      provide: Pay_TOKEN,
+      useClass: CategoryChangeService,
+    }
+  ]
 })
 export class CategorychangeComponent {
 
@@ -24,6 +40,27 @@ export class CategorychangeComponent {
   payPeriodId: number = 0;
   payperiods: String = '';
   payPeriodTypefromParentall: string = '';
+  lot_number!: number;
+  categorySearch: any;
+  showTable = false;
+  dataSource = new MatTableDataSource<any>();
+  @ViewChild(MatSort) sort!: MatSort;
+  ProcessCategory: any;
+
+  constructor(private _decrypt: EncryptionService, private _sessionStoreage: SessionStorageService, private service: CategoryChangeService) { }
+
+  uploadDisplayedColumns: string[] = [
+    'slNo', 'companycode', 'lotNumber', 'payrollinputtype', 'update'];
+
+  uploadedData: any[] = []; // No mock data
+
+  uploadedDataSource = new MatTableDataSource<any>(this.uploadedData);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.uploadedDataSource.paginator = this.paginator;
+  }
 
   handleCompanyEvent(company) {
     this.selectedCompanyId = company.companyId;
@@ -34,24 +71,80 @@ export class CategorychangeComponent {
 
   handlePayperiodEvent(payperiod: Payperiodclass) {
     this.payPeriod = payperiod;
+    console.log("payperiod", this.payPeriod)
     this.payPeriodId = payperiod.payfrequencyid;
     this.payperiods = payperiod.payPeriod;
   }
 
   ngOnInit() {
     this.payPeriodTypefromParentall = "All";
+    this.BindProcessCategory();
   }
+
+  BindProcessCategory() {
+    this.service.GetProcessCategory().subscribe({
+      next: res => { this.ProcessCategory = res.Data },
+      error: err => { console.log(err) }
+    })
+  }
+
   onSearch() {
-    // this.isLoading = true;
     if (!this.selectedCompanyId) {
       alert('Please select Company Code');
-      // this.isLoading = false;
       return;
     }
-    if (!this.payPeriod) {
-      alert('please select Payperiod');
+    if (!this.payPeriod || !this.payPeriod.payPeriod) {
+      alert('Please select Pay Period');
       return;
+    }
+    this.showTable = true;
+    const payload = {
+      CompanyID: this.selectedCompanyId,
+      PayPeriod: this.payPeriodId,
+      LotNumber: this.lot_number,
+      Revised: 0,
+      Flag: "s"
+
+    }
+    this.service.SearchCategory(payload).subscribe({
+      next: (res) => {
+        this.isLoading = false;
+        this.categorySearch = res.Data;
+        if (this.categorySearch && this.categorySearch.length > 0) {
+          this.dataSource = new MatTableDataSource(this.categorySearch);
+          this.dataSource.paginator = this.paginator;
+          this.dataSource.sort = this.sort;
+          this.uploadDisplayedColumns = [
+            'slNo', 'companycode', 'lotNumber', 'payrollinputtype', 'update'];
+        }
+      },
+      error: (err) => {
+        this.isLoading = false;
+        console.error('Error loading salary release data', err);
+      },
+    });
+
+  }
+  onUpdate(){
+    const payload = {
+      CompanyID: this.selectedCompanyId,
+      PayPeriod: this.payPeriodId,
+      LotNumber: this.lot_number,
+      Revised: 0,
+      Flag: "U"
     }
 
+     this.service.SearchCategory(payload).subscribe({
+      next: (res) => {
+        this.isLoading = false;
+        alert("Updated Successfully")
+        this.onSearch();
+        
+      },
+      error: (err) => {
+        this.isLoading = false;
+        console.error('Error loading salary release data', err);
+      },
+    });
   }
 }
