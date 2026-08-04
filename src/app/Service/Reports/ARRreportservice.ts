84fe7a3cd10dc978a96da50f597d4aee1c4eea87
selvaraju.rethinam@quesscorp.com <SelvarajuRethinam@QC6065-L>
2026-08-04 15:58:15 +0530

    Assignment page changes completed

--- src/app/Service/Reports/ARRreportservice.ts
+++ src/app/Service/Reports/ARRreportservice.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../../environments/environment';
 import { APIResponse } from '../../Models/apiresponse';
 import { IARReportService } from '../../Repository/Reports/IARReoprtservice';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
     providedIn: 'root'
